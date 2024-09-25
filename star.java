19db09bcc944f9b32927aef04865364ce5e0f308
govindbodda <rajubodda91@gmail.com>
2024-09-25 15:56:13 +0530

    Create star.java

---
+++ star.java
@@ -0,0 +1,13 @@
+class star
+  {
+    public static void main(String args[])
+    {
+      int i,n=5,j;
+      for(i=0;i<n;i++)
+        {
+          for(j=0;j<i;j++)
+          System.out.print("*");
+          System.out.println("");
+        }
+    }
+  }
